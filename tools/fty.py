e10d179aeb7b9ffed3b945da8a0c616e04248649
qist <87984115@qq.com>
2026-02-01 17:26:21 +0800

    fix

--- tools/fty.py
+++ tools/fty.py
@@ -7,10 +7,6 @@ import json
 session = requests.Session()
 COMMON_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
 }
 session.headers.update(COMMON_HEADERS)
 session.cookies.set("visited", "1")  # 提高拟真度
